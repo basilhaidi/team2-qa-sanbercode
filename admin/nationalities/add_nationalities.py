0a114d0221c7569e823e871596fe6ed14c7aa310
Yuda Darma <76606722+yudadarma11@users.noreply.github.com>
2022-12-19 12:44:28 +0700

    Add files via upload

---
+++ admin/nationalities/add_nationalities.py
@@ -0,0 +1,44 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestLogin(unittest.TestCase): 
+
+    def setUp(self): 
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    #User Berhasil Login
+    def test_success_login(self): 
+        # login
+        browser = self.browser #buka web browser
+        browser.maximize_window()
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") # buka situs
+        time.sleep(3)
+        browser.find_element(By.CSS_SELECTOR,"#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(2) > div > div:nth-child(2) > input").send_keys("Admin") # isi nama by Full XPATH
+        time.sleep(1)
+        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123") # isi password by XPATH, #email tidak boleh sama
+        time.sleep(1)
+        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol login by XPATH
+        time.sleep(3)
+
+        #Open page Admin
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span").click() # klik tombol login by XPATH
+        time.sleep(3)
+
+        #Open Tab Nationalities
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[5]/a").click() # klik tombol login by XPATH
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/div[1]/div/button").click() # klik tombol login by XPATH
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("COBA") # klik tombol login by XPATH
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]").click() # klik tombol login by XPATH
+        time.sleep(7)
+
+    def tearDown(self): 
+        self.browser.close() 
+
+if __name__ == "__main__": 
+    unittest.main()
